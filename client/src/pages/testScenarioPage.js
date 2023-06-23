24d711f5393f467db4caa3cf789a91fc6f52127a
Miles Tucker <milestucker@Miless-MacBook-Pro.local>
2023-06-22 21:09:59 -0400

    add new tests to test table

--- client/src/pages/testScenarioPage.js
+++ client/src/pages/testScenarioPage.js
@@ -1,5 +1,6 @@
 import { useCallback, useMemo, useState } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { subDays, subHours } from 'date-fns';
 import ArrowDownOnSquareIcon from '@heroicons/react/24/solid/ArrowDownOnSquareIcon';
 import ArrowUpOnSquareIcon from '@heroicons/react/24/solid/ArrowUpOnSquareIcon';
@@ -37,14 +38,7 @@ const data = [
   },
 ];
 
-const useCustomers = (page, rowsPerPage) => {
-  return useMemo(
-    () => {
-      return applyPagination(data, page, rowsPerPage);
-    },
-    [page, rowsPerPage]
-  );
-};
+
 
 const useCustomerIds = (customers) => {
   return useMemo(
@@ -56,11 +50,52 @@ const useCustomerIds = (customers) => {
 };
 
 const Page = () => {
+
+  const router = useRouter();
+  const response = JSON.parse(router.query.response || '{}');
+
+
+  const scenarios = response.result.content.split("\n");
+
+  const useScenarios = (page, rowsPerPage) => {
+    return useMemo(
+      () => {
+        return applyPagination(scenarios, page, rowsPerPage);
+      },
+      [page, rowsPerPage]
+    );
+  };
+
+  const handleSaveTests = async (event) => {
+    event.preventDefault();
+
+    const selectedItems = customersSelection.selected.map((selection) => {
+      return { content: selection };
+    });
+    
+      // Send the API request
+      const response = await fetch("http://localhost:5000/api/saveTestScenarios", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(selectedItems),
+      });
+
+      if (response.ok) {
+        router.push('/viewTest');
+      } else {
+        // Handle the error case
+        console.log('API request failed');
+      }
+
+  }
+
+
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
-  const customers = useCustomers(page, rowsPerPage);
-  const customersIds = useCustomerIds(customers);
-  const customersSelection = useSelection(customersIds);
+  const scenarioList = useScenarios(page, rowsPerPage);
+  const customersSelection = useSelection(scenarioList);
   const handlePageChange = useCallback(
     (event, value) => {
       setPage(value);
@@ -74,6 +109,8 @@ const Page = () => {
     },
     []
   );
+
+
 
   return (
     <>
@@ -105,7 +142,7 @@ const Page = () => {
             <SelectScenario />
             <TestScenarios
               count={data.length}
-              items={customers}
+              items={scenarioList}
               onDeselectAll={customersSelection.handleDeselectAll}
               onDeselectOne={customersSelection.handleDeselectOne}
               onPageChange={handlePageChange}
@@ -118,10 +155,9 @@ const Page = () => {
             />
           </Stack>
           <div align="center">
-            <Button variant="contained" size="small" align="center" sx={{ mt: 2}}
-              href="/testStepsPage"
+            <Button variant="contained" size="small" align="center" sx={{ mt: 2}} onClick={handleSaveTests}
             >
-              Next
+              Save Tests
             </Button>
           </div>
         </Container>
--- server/index.js
+++ server/index.js
@@ -5,46 +5,19 @@ const app = express()
 const bodyParser = require('body-parser');
 const cors = require('cors'); // Import the cors middleware
 supabase = require("./SupabaseServer.js")
+const { Configuration, OpenAIApi } = require("openai");
 
 
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
-app.post("/api/createTestScenarios", async (req, res) => {
-    console.log("test scenarios created");
-
-    const { data, error } = await supabase
-    .from('test_plan')
-    .insert([
-      { test_plan_name: 'test plan 1' },
-    ])
-  // return { data, error };
-
-  console.log("error: " + JSON.stringify(error));
-  console.log("data: " + JSON.stringify(data));
-
-
-
-  const { data2, error2 } = await supabase
-  .from('test_case')
-  .insert([
-    { content: 'test case 1'},
-  ])
-// return { data, error };
-
-console.log("error: " + JSON.stringify(error2));
-console.log("data: " + JSON.stringify(data2));
-
-
-    res.json({
-      result: "hello world"
-      })
-   
-  });
 
 app.get("/api/getTestScenarios", cors(), async (req, res) => {
 
@@ -57,6 +30,50 @@ app.get("/api/getTestScenarios", cors(), async (req, res) => {
       tests: data
       })
   });
+
+  app.post("/api/createTestScenarios", async (req, res) => {
+
+  
+    const requestBody = JSON.stringify(req.body);
+
+    console.log("body: " + requestBody);
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages:[
+        {"role": "system", "content": "You are an assistant that is a quality assurance tester. Your task is to create test case scenarios for based on a product’s user story and the acceptance criteria. The test case scenarios should include happy paths and nonhappy paths, and even edge cases."},
+        {"role": "user", "content": "User story: As a Netflix user, I can pause my show and can return to where I left off on the show when I turn on netflix again.\nAcceptance criteria:\nWhen I click pause, netflix will save the scene where I stopped watching the show.\nIf there is a network error when I try to pause, netflix will use the last saved index of my show and will continue from that point when I return to watch netflix.\nWhen I press play, netflix will resume the show from the same place when I paused it."},
+        {"role": "assistant", "content": "Test Case Scenarios:\nHappyPath:If a user fast forwards through a tv show and pauses, netflix will save the index where the user paused successfully.\nIf a user is playing a show at regular speed and pauses the show, netflix will save the index where the user paused successfully.\nIf a user exits netflix without clicking the pause button, netflix will will save the index and video frame where the user last watched successfully.\nIf a user rewinds through a tv show and pauses, netflix will save the index where the user paused successfully.\nIf a user rewinds through a tv show and but never pauses the show, but instead just exits netflix, netflix will index where the user last watched before they started rewinding the show."},
+        {"role": "user", "content": requestBody}
+        ],
+        temperature: 0.2,
+    });
+    console.log(completion.data.choices[0].message);
+    res.json({
+        result: completion.data.choices[0].message
+        })
+  
+  
+})
+
+app.post("/api/saveTestScenarios", async (req, res) => {
+
+  console.log("body: " + JSON.stringify(req.body));
+
+  const { data, error } = await supabase
+  .from('test_case')
+  .insert(
+    req.body
+  )
+
+  console.log("error: " + JSON.stringify(error));
+  console.log("data: " + JSON.stringify(data));
+
+
+  res.json({
+    result: "saved"
+    })
+})
+
 
   if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets

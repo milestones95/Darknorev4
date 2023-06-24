c9745001db0a6a83829c05ea2a67c7ca7d2d435b
tyree <tyreeostevenson@gmail.com>
2023-06-24 12:21:56 -0400

    add test wizard and fixed navbar

---
+++ client/src/sections/layouts/new-test-wizard.js
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import { useState, useEffect } from 'react';
+
+const steps = [
+  '',
+  '',
+  '',
+];
+
+export default function NewTestWizard() {
+  const [currentSteps, setCurrentSteps] = useState((1));
+
+  useEffect(() => {
+    if (window.location.pathname.startsWith('/createTests'))
+      setCurrentSteps((1))
+    if (window.location.pathname.startsWith('/testScenarioPage'))
+        setCurrentSteps((1,2))
+    if (window.location.pathname.startsWith('/testStepsPage'))
+        setCurrentSteps((1,2,3))
+
+  }, [window.location.pathname]);
+
+
+  return (
+    <Box sx={{ width: '25%'}}>
+      <Stepper activeStep={currentSteps} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel />
+          </Step>
+        ))}
+      </Stepper>
+    </Box>
+  );
+}

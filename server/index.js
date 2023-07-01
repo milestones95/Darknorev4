303443ed5f8be2f7b59cf560e21fabb6d4b2ade0
Miles Tucker <milestucker@Miless-MacBook-Pro.local>
2023-07-01 15:26:01 -0400

    move path decllaration higher

--- server/index.js
+++ server/index.js
@@ -104,13 +104,15 @@ app.post("/api/saveTestScenarios", async (req, res) => {
 
 
   if (process.env.NODE_ENV === 'production') {
+
+    const path = require('path');
+
     // Exprees will serve up production assets
     if (process.env.NODE_ENV === 'production') {
       // Express will serve up production assets
       app.use(express.static(path.join(__dirname, '../client/build')));
     
       // Express serve up index.html file if it doesn't recognize the route
-      const path = require('path');
       app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
       });

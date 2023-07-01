350a30e055877acd7d1423cfa8bae10a0249963e
Miles Tucker <milestucker@Miless-MacBook-Pro.local>
2023-07-01 15:18:21 -0400

    change build path in index.js

--- server/index.js
+++ server/index.js
@@ -105,12 +105,16 @@ app.post("/api/saveTestScenarios", async (req, res) => {
 
   if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('../clientblahhhblee/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+    if (process.env.NODE_ENV === 'production') {
+      // Express will serve up production assets
+      app.use(express.static(path.join(__dirname, '../client/build')));
+    
+      // Express serve up index.html file if it doesn't recognize the route
+      const path = require('path');
+      app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
+      });
+    }
+    
   }
   app.listen(process.env.PORT || 5000, () => { console.log("server started on port 5000")})

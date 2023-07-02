09378e830a6e73cd3a43df6e287573fdd47e10d4
tyree <tyreeostevenson@gmail.com>
2023-07-02 09:37:42 -0400

    ythy

--- server/index.js
+++ server/index.js
@@ -103,6 +103,13 @@ app.post("/api/saveTestScenarios", async (req, res) => {
     })
 })
 
+// Helper function to get folders recursively from a given directory
+function getFolders(dir) {
+  return fs
+    .readdirSync(dir, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+}
 
   if (process.env.NODE_ENV === 'production') {
 
@@ -111,13 +118,18 @@ app.post("/api/saveTestScenarios", async (req, res) => {
       // Express will serve up production assets
       app.use(express.static('../client/build'));
 
-      console.log("directorypath before: "+ path.join(__dirname, '../client/build'))
-    
+      app.get('/', (req, res) => {
+        const rootDir = __dirname; // The root directory of your JavaScript project
+        const folders = getFolders(rootDir);
+        res.json(folders);
+      });
+
+
       // Express serve up index.html file if it doesn't recognize the route
       app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
       });
     }
-    
+
   }
   app.listen(process.env.PORT || 5000, () => { console.log("server started on port 5000")})

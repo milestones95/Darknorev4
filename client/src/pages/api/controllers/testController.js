c6a9779b1f0a2a2dd36194896c2b3da048aca754
amanforindia <amanforindia@gmail.com>
2023-07-15 11:10:17 +0530

    Add SupabaseServer

--- client/src/pages/api/controllers/testController.js
+++ client/src/pages/api/controllers/testController.js
@@ -1,5 +1,5 @@
 const { Configuration, OpenAIApi } = require("openai");
-const supabase = require("../SupabaseClient.js")
+const supabase = require("../SupabaseServer.js")
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,

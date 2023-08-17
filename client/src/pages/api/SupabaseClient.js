b6ebb8ce8cf7e30ad7b67a3eb76e5cd6eb272ba3
ABHINAY CHATURVEDI <108412199+abhinay-chaturvedi@users.noreply.github.com>
2023-08-17 22:47:36 +0530

    change supabse keys (#14)

--- client/src/pages/api/SupabaseClient.js
+++ client/src/pages/api/SupabaseClient.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 import { createClient } from '@supabase/supabase-js'
 
-const SUPABASE_URL = "https://abniisbhpxstkyezeosc.supabase.co"
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFibmlpc2JocHhzdGt5ZXplb3NjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcwMTkyMDAsImV4cCI6MjAwMjU5NTIwMH0.Xa3VzYTd0V5-p5IqzjKGcUH0y-iFBzrYxgAqNmqC2dM'
+// const SUPABASE_URL = "https://abniisbhpxstkyezeosc.supabase.co"
+// const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFibmlpc2JocHhzdGt5ZXplb3NjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODcwMTkyMDAsImV4cCI6MjAwMjU5NTIwMH0.Xa3VzYTd0V5-p5IqzjKGcUH0y-iFBzrYxgAqNmqC2dM'
+const SUPABASE_URL = "https://jutmheobdppfdaoilaaq.supabase.co"
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1dG1oZW9iZHBwZmRhb2lsYWFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA3ODc5MjEsImV4cCI6MjAwNjM2MzkyMX0.3p9bhZohXL_7HXhQrALXFa8TUUjMdPAbCdU5WSTj3is'
 // const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL
 // console.log("🚀 ~ file: SupabaseClient.js:6 ~ SUPABASE_URL:", SUPABASE_URL)
 // const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY
